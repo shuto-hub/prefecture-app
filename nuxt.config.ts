b5e23698d42583800d294e841158294c21eac86d
Your Name <you@example.com>
2023-06-25 16:03:36 +0900

    [add]vue-tscの設定を追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  typescript: {
+    shim: false,
+    strict: true,
+    typeCheck: false
+  }
 });
