cb1351258c3893b6844bc564c063f41d26ed732f
Your Name <you@example.com>
2023-06-27 22:16:57 +0900

    [fix]lintfix実行に伴う修正

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <h1 class="app-title">都道府県別総人口推移グラフ</h1>
-    <Prefecture />
-    <Chart />
+    <PrefectureSection />
+    <ChartSection />
   </div>
 </template>
 <script lang="ts" setup></script>
